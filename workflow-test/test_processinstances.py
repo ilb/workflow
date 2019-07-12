c1bef5ae0471b26bd85466a4398c34b4de947451
slavb <slavb@ilb.ru>
2019-07-12 11:02:43 +0000

    python tests

--- workflow-test/test_processinstances.py
+++ workflow-test/test_processinstances.py
@@ -15,10 +15,10 @@ class TestProcessInstances(unittest.TestCase):
         self.configuration.username = "ide"
         self.configuration.password = "123"
         self.configuration.debug=True
-        self.workflow_api = workflow.DefaultApi(workflow.ApiClient(self.configuration))
+        self.workflow_api = workflow.DefaultApi(workflow.ApiClient(configuration=self.configuration,header_name="Authorization",header_value="Basic c2xhdmI6MTIz"))
         
     def test_createProcessInstanceAndNext(self):
-        processInstanceId = self.workflow_api.create_process_instance(process_definition_id="stockvaluation_fairpricecalc")
+        processInstanceId = self.workflow_api.create_process_instance_and_next_with_http_info(process_definition_id="stockvaluation_fairpricecalc",body={})
         print('processInstanceId', processInstanceId)
 
 if __name__ == '__main__':
