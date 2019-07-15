de8ee4d1376744c23513fe5931c75f45914042fb
slavb <slavb@ilb.ru>
2019-07-15 08:55:08 +0000

    form url fix

--- workflow-js/src/App.js
+++ workflow-js/src/App.js
@@ -5,37 +5,35 @@ import ActivityFormContainer from './components/ActivityForm';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ProcessMenuContainer from './components/ProcessMenu';
 import ProcessSelectorContainer from './components/ProcessSelector';
-import queryString from 'query-string'
 
 
 function Index() {
     return (<div className="app">
-        <Link to="/activityForm?processInstanceId=5801_stockvaluation_stockvaluation_fairpricecalc&activityInstanceId=8302_5801_stockvaluation_stockvaluation_fairpricecalc_stockvaluation_fairpricecalc_input">TEST PROCESS</Link>
+        <Link to="/processInstances/5801_stockvaluation_stockvaluation_fairpricecalc/activityInstances/8302_5801_stockvaluation_stockvaluation_fairpricecalc_stockvaluation_fairpricecalc_input/activityForm">TEST PROCESS</Link>
         |
-        <Link to="/activityForm?processInstanceId=5602_stockvaluation_stockvaluation_fairpricecalc&activityInstanceId=8008_5602_stockvaluation_stockvaluation_fairpricecalc_stockvaluation_fairpricecalc_check">TEST PROCESS2</Link>
+        <Link to="/processInstances/5602_stockvaluation_stockvaluation_fairpricecalc/activityInstances/8008_5602_stockvaluation_stockvaluation_fairpricecalc_stockvaluation_fairpricecalc_check/activityForm">TEST PROCESS2</Link>
         <ProcessSelectorContainer/>
     </div>
             );
 }
 
-function ActivitFormIndex( { match }) {
-    console.log('match', match);
-    const values = queryString.parse(match.url)
-    console.log('values', values);
+function ActivityFormIndex( { match }) {
+    //console.log('match.params', match.params);
     return (
         <div className="app">
             <Index/>
-            <ActivityFormContainer processInstanceId={values.processInstanceId} activityInstanceId={values.activityInstanceId}/>
+            <ActivityFormContainer processInstanceId={match.params.processId} activityInstanceId={match.params.activityId}/>
         </div>
     );
 }
-// <Route path="/processes/:processInstanceId" component={ActivitFormIndex} />
+// <Route path="/processes/:processId" component={ActivityFormIndex} />
 function App() {
     return (
             <Router>
                 <div>
                     <Route path="/" exact component={Index} />
-                    <Route path="/activityForm" component={ActivitFormIndex} />
+                    
+                    <Route path="/processInstances/:processId/activityInstances/:activityId/activityForm" component={ActivityFormIndex} />
                 </div>
             </Router>
             );
