ce188b88378ee9534147da8240ecdb108939c91c
slavb <slavb@ilb.ru>
2019-07-22 05:07:30 +0000

    configure cert

--- workflow-js/src/setupProxy.js
+++ workflow-js/src/setupProxy.js
@@ -1,22 +1,34 @@
 const proxy = require('http-proxy-middleware');
+
+//const homedir = require('os').homedir();
 const fs = require('fs');
-const keytar = require('keytar');
+const path = require('path');
+const xpath = require('xpath');
+const DOMParser = require('xmldom').DOMParser;
+
+const configpath = path.resolve(path.join(process.env.HOME, '.config/context.xml'))
+
+const xml = fs.readFileSync(configpath, 'utf8');
+const doc = new DOMParser().parseFromString(xml)
+const certpath = xpath.select1("//Environment[@name='ru.bystrobank.apps.workflow.certfile']/@value", doc).value;
+const cert = fs.readFileSync(certpath);
+const passphrase = xpath.select1("//Environment[@name='ru.bystrobank.apps.workflow.cert_PASSWORD']/@value",doc).value;
 
 module.exports = async function (app) {
-    const homedir = require('os').homedir();
-    const passphrase = await keytar.getPassword('.certs', 'my.p12');
-    //disable password log logLevel: 'warn'
 
     var options = {
         target: {
             host: 'devel.net.ilb.ru',
             port: 443,
             protocol: 'https:',
-            pfx: fs.readFileSync(homedir+'/.certs/my.p12'),
+            key: cert,
+            cert: cert,
             passphrase: passphrase
+
         },
-        logLevel: 'warn',
+        logLevel: 'info',
         changeOrigin: true
     };
+
     app.use(proxy('/workflow-web', options));
 };
