764bd9ada4e359ffe814f8c586b1b05275d6dea4
slavb <slavb@ilb.ru>
2019-09-18 03:54:40 +0000

    delete old workflow-js

--- workflow-js/src/Config.js
+++
@@ -1,5 +0,0 @@
-import { ApiClient as WorkflowApiClient} from '@ilb/workflow-api/dist';
-import { ApiClient as DossierApiClient} from '@ilb/filedossier-api/dist';
-
-WorkflowApiClient.instance.basePath = '/workflow-web/web/v2';
-DossierApiClient.instance.basePath = '/workflow-web/web/v2';
