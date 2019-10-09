204545174a56f7f60f67ce2678998bcdf5238d50
krasnoperov <krasnoperov@ilb.ru>
2019-10-09 10:09:30 +0000

    T290680: Прототип АРМ "Оценка залога"

--- workflow-js/pages/api/dossier/multipart/[method].js
+++ workflow-js/pages/api/dossier/multipart/[method].js
@@ -1,7 +1,13 @@
 import { createDossierApi } from '@ilb/filedossier-js/conf/config';
-import { executeApi } from '@ilb/filedossier-js/classes/jsApiAutoProxy';
+import { executeFileApi } from '@ilb/filedossier-js/classes/jsApiAutoProxy';
 
 export default async (req, res) => {
   const apiDossier = createDossierApi((req && req.headers) ? req.headers['x-remote-user'] : null);
-  executeApi(apiDossier, req, res);
+  executeFileApi(apiDossier, req, res);
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
